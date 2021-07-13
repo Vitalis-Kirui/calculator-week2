cfd8a1e8ec7ab0bd66a8bfbfd7a5f37830fa4a08
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-07-13 15:42:55 +0300

    pushing js code for other arithmetic operations

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
+// Addition
+
 var add = function(number1, number2) {
     return number1 + number2;
   };
-  var number1 = parseInt(prompt("Enter a number:"))
-  var number2 = parseInt(prompt("Enter another number:"))
+  var number1 = parseInt(prompt("(Addition) Enter first number:"))
+  var number2 = parseInt(prompt("(Addition) Enter second number:"))
   alert(add(number1, number2));
+
+//   Subtraction
+var subtract = function(number1, number2) {
+    return number2 - number1;
+  };
+  var number1 = parseInt(prompt("(Subtraction) Enter first number:"))
+  var number2 = parseInt(prompt("(Subtraction) Enter second number:"))
+  alert(subtract(number1, number2));
+
+//   Division
+var divide = function(number1, number2) {
+    return number2 / number1;
+  };
+  var number1 = parseInt(prompt("(Division) Enter first number:"))
+  var number2 = parseInt(prompt("(Division) Enter second number:"))
+  alert(divide(number1, number2));
+
+//   Multiplication
+var multiply = function(number1, number2) {
+    return number1 * number2;
+  };
+  var number1 = parseInt(prompt("(Multiplication) Enter first number:"))
+  var number2 = parseInt(prompt("(Multiplication) Enter second number:"))
+  alert(multiply(number1, number2));
